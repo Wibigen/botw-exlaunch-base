9c0dd48f97f732948aa4145a5c1a5f6067b9d366
WibiDev <wibigen@protonmail.com>
2026-07-05 10:46:21 +0200

    Update main.cpp

--- source/program/main.cpp
+++ source/program/main.cpp
@@ -18,13 +18,25 @@ HOOK_DEFINE_REPLACE(myReplaceHook) {
     }
 };
 
+// This will hook the main function of botw (nnMain)
+HOOK_DEFINE_TRAMPOLINE(mainHook) {
+    static void Callback(void* thisPtr) { // Note that if there aren't any argument in the base function, you should write `void* [a name]` !
+        Orig(thisPtr);
+
+        // your custom code
+    }
+};
+
+
 extern "C" void exl_main(void* x0, void* x1) {
     // Setup hooking environment
     exl::hook::Initialize();
 
-    // Install the hooks at the provided offset
+    // Install the hooks at the provided offset (offsets can be found in uking_function.csv, at this link : https://github.com/zeldaret/botw/blob/master/data/uking_functions.csv)
     myTrampolineHook::InstallAtOffset(0x9a2bd5);
     myReplaceHook::InstallAtOffset(0x9a2bd6);
+
+    mainHook::InstallAtOffset(0x7d5fe4);
 }
 
 // There are more things in exlaunch, these are only the basics for BOTW modding
